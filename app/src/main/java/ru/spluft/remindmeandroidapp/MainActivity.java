66af99d5c9f63875c108f8295aebf4ce3f2c8ed1
Nikolay <spluft@ya.ru>
2018-05-02 16:17:21 +0300

    add menu in navigationView

--- app/src/main/java/ru/spluft/remindmeandroidapp/MainActivity.java
+++ app/src/main/java/ru/spluft/remindmeandroidapp/MainActivity.java
@@ -3,6 +3,7 @@ package ru.spluft.remindmeandroidapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,14 +12,18 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+    
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +51,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
     }
 }
