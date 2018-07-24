f376aa2c88635b29c87c7893003329fa39f41b1d
spluft <spluft@ya.ru>
2018-07-24 21:00:30 +0300

    mock data for all tabs

--- app/src/main/java/ru/spluft/remindmeandroidapp/fragment/IdeaFragment.java
+++ app/src/main/java/ru/spluft/remindmeandroidapp/fragment/IdeaFragment.java
@@ -4,11 +4,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.spluft.remindmeandroidapp.R;
+import ru.spluft.remindmeandroidapp.adapter.RemindListAdapter;
+import ru.spluft.remindmeandroidapp.dto.RemindDTO;
 
 public class IdeaFragment extends AbstractTabFragment {
 
@@ -28,7 +35,23 @@ public class IdeaFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
+
+        RecyclerView rv = view.findViewById(R.id.recyclerview);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(createMock()));
+
         return view;
+    }
+
+    private List<RemindDTO> createMock() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+        return data;
     }
 
     public void setContext(Context context) {
